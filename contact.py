ef74da6629a5877855679ec9934b7c3f494c2d9e
lefej <lefej@rambler.ru>
2019-10-21 21:38:27 +0300

    добавлен класс contact

---
+++ contact.py
@@ -0,0 +1,31 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, address, company, home_phone,
+                     mobile_phone, work_phone, fax, email, email2, email3, site, birthday_year, aniversary_day,
+                     birthday_day, birthday_month, aniversary_month, aniversary_year, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.address = address
+        self.company = company
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.site = site
+        self.birthday_year = birthday_year
+        self.aniversary_day = aniversary_day
+        self.birthday_day = birthday_day
+        self.birthday_month = birthday_month
+        self.aniversary_month = aniversary_month
+        self.aniversary_year = aniversary_year
+        self.address2 = address2
+        self.phone2 = notes
+        self.notes = notes
+
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -13,12 +14,37 @@ class TestAddContact(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_add_contact(self):
+    def test_add_contact1(self):
         wd = self.wd
         self.open_page(wd)
-        self.login(wd)
+        self.login(wd, username = "admin", password = "secret")
         self.init_add_contact(wd)
-        self.fill_contact(wd)
+        self.fill_contact(wd, Contact(firstname = "Олеся", middlename = "Павловна", lastname = "Лисовская", nickname = "lefej", title = "тест", address = "г. Москва, ул. Раменки, д.25",
+                          company = "НИИ Восход", home_phone = "555555", mobile_phone = "444444", work_phone = "33333", fax = "22222",
+                          email = "lefej@rambler.ru", email2 = "olesalis@gmail.com",
+                          email3 = "test@mail.ru", site = "test.com",
+                          birthday_year = "1990", aniversary_day = "31", birthday_day = "30", birthday_month = "July",
+                          aniversary_month = "August", aniversary_year = "2000",
+                          address2 = "ул. Удальцова, д.85", phone2 = "тест2", notes = "тест тест тест 7777"))
+        self.confirm_add_contact(wd)
+        self.go_home(wd)
+        self.logout(wd)
+
+    def test_add_contact2(self):
+        wd = self.wd
+        self.open_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.init_add_contact(wd)
+        self.fill_contact(wd, Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="ivanov",
+                                      title="тест", address="г. Москва, ул. Ленина, д.25",
+                                      company="АО Сбербанк", home_phone="555555", mobile_phone="444444",
+                                      work_phone="33333", fax="22222",
+                                      email="ivanov@rambler.ru", email2="ivanov@gmail.com",
+                                      email3="test@mail.ru", site="test.com",
+                                      birthday_year="2000", aniversary_day="2", birthday_day="1",
+                                      birthday_month="January",
+                                      aniversary_month="August", aniversary_year="2000",
+                                      address2="ул. Почтовая, д.85", phone2="тест2", notes="тест тест тест тест тест 999555"))
         self.confirm_add_contact(wd)
         self.go_home(wd)
         self.logout(wd)
@@ -39,33 +65,33 @@ class TestAddContact(unittest.TestCase):
             "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
         wd.find_element_by_xpath("//body").click()
 
-    def fill_contact(self, wd):
+    def fill_contact(self, wd, contact):
         # Заполнение нового контакта
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Олеся")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Павловна")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Лисовская")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("lefej")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         # wd.find_element_by_name("photo").clear()
         # wd.find_element_by_name("photo").send_keys("C:\\unnamed.jpg")
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("тест")
+        wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("НИИ Восход")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("г. Москва, ул. Раменки, д.25")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("theform").click()
@@ -74,65 +100,65 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("555555")
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("444444")
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("33333")
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("22222")
+        wd.find_element_by_name("fax").send_keys(contact.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("lefej@rambler.ru")
+        wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("olesalis@gmail.com")
+        wd.find_element_by_name("email2").send_keys(contact.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("test@mail.ru")
+        wd.find_element_by_name("email3").send_keys(contact.email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("test.com")
+        wd.find_element_by_name("homepage").send_keys(contact.site)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("30")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birthday_day)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[32]").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("July")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birthday_month)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[41]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1990")
+        wd.find_element_by_name("byear").send_keys(contact.birthday_year)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("31")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aniversary_day)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[33]").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("August")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.aniversary_month)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[42]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2000")
+        wd.find_element_by_name("ayear").send_keys(contact.aniversary_year)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("ул. Удальцова, д.85")
+        wd.find_element_by_name("address2").send_keys(contact.address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("тест2")
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("тест тест тест 7777")
+        wd.find_element_by_name("notes").send_keys(contact.notes)
 
     def init_add_contact(self, wd):
         # Инициализация создания контакта
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd, username="admin", password="secret"):
+    def login(self, wd, username, password):
         # Логин
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
