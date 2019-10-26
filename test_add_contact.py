332f15c79393e784ea1072a5bf85a28f069a1858
lefej <lefej@rambler.ru>
2019-10-26 15:27:08 +0300

    перенесли на pytest контакты

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,183 +1,37 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import pytest
+from application import Application
 from contact import Contact
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
     
-    def test_add_contact1(self):
-        wd = self.wd
-        self.open_page(wd)
-        self.login(wd, username = "admin", password = "secret")
-        self.init_add_contact(wd)
-        self.fill_contact(wd, Contact(firstname = "Олеся", middlename = "Павловна", lastname = "Лисовская", nickname = "lefej", title = "тест", address = "г. Москва, ул. Раменки, д.25",
+def test_add_contact1(app):
+        app.login(username = "admin", password = "secret")
+        app.create_contact(Contact(firstname = "Олеся", middlename = "Павловна", lastname = "Лисовская", nickname = "lefej", title = "тест", address = "г. Москва, ул. Раменки, д.25",
                           company = "НИИ Восход", home_phone = "555555", mobile_phone = "444444", work_phone = "33333", fax = "22222",
                           email = "lefej@rambler.ru", email2 = "olesalis@gmail.com",
                           email3 = "test@mail.ru", site = "test.com",
-                          birthday_year = "1990", aniversary_day = "31", birthday_day = "30", birthday_month = "July",
+                          birthday_year = "1990", aniversary_day = "28", birthday_day = "30", birthday_month = "July",
                           aniversary_month = "August", aniversary_year = "2000",
                           address2 = "ул. Удальцова, д.85", phone2 = "тест2", notes = "тест тест тест 7777"))
-        self.confirm_add_contact(wd)
-        self.go_home(wd)
-        self.logout(wd)
+        app.logout()
 
-    def test_add_contact2(self):
-        wd = self.wd
-        self.open_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.init_add_contact(wd)
-        self.fill_contact(wd, Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="ivanov",
+def test_add_contact2(app):
+        app.login(username="admin", password="secret")
+        app.create_contact(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="ivanov",
                                       title="тест", address="г. Москва, ул. Ленина, д.25",
                                       company="АО Сбербанк", home_phone="555555", mobile_phone="444444",
                                       work_phone="33333", fax="22222",
                                       email="ivanov@rambler.ru", email2="ivanov@gmail.com",
                                       email3="test@mail.ru", site="test.com",
-                                      birthday_year="2000", aniversary_day="2", birthday_day="1",
+                                      birthday_year="2000", aniversary_day="22", birthday_day="11",
                                       birthday_month="January",
                                       aniversary_month="August", aniversary_year="2000",
                                       address2="ул. Почтовая, д.85", phone2="тест2", notes="тест тест тест тест тест 999555"))
-        self.confirm_add_contact(wd)
-        self.go_home(wd)
-        self.logout(wd)
 
-    def logout(self, wd):
-        # Логаут
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-
-    def go_home(self, wd):
-        # Переход на главную страницу
-        wd.find_element_by_link_text("home").click()
-
-    def confirm_add_contact(self, wd):
-        # Подтверждение создания контакта
-        wd.find_element_by_xpath("//input[@name='submit']").click()
-        wd.find_element_by_xpath("//body").click()
-
-    def fill_contact(self, wd, contact):
-        # Заполнение нового контакта
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("photo").clear()
-        wd.find_element_by_name("photo").send_keys("C:\\unnamed.jpg")
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.site)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birthday_day)
-        wd.find_element_by_xpath("//select[@name='bday']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birthday_month)
-        wd.find_element_by_xpath("//select[@name='bmonth']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.birthday_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aniversary_day)
-        wd.find_element_by_xpath("//select[@name='aday']").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.aniversary_month)
-        wd.find_element_by_xpath("//select[@name='amonth']").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.aniversary_year)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-
-    def init_add_contact(self, wd):
-        # Инициализация создания контакта
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        # Логин
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@type='submit']").click()
-
-    def open_page(self, wd):
-        # Открытие страницы
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+        app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,94 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, re
+import pytest
 from group import Group
+from application import Application
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
     
-    def test_add_group(self):
-        self.login(username = "admin", password = "secret")
-        self.create_group(Group(name= "группа", header = "тест", footer = "фывафывафыафыа"))
-        self.logout()
+def test_add_group(app):
+        app.login(username = "admin", password = "secret")
+        app.create_group(Group(name= "группа", header = "тест", footer = "фывафывафыафыа"))
+        app.logout()
 
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        # Логаут
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_xpath("//html").click()
-
-    def return_to_groups(self):
-        wd = self.wd
-        # Возврат на страницу Группы
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups()
-        # Создание новой группы
-        wd.find_element_by_name("new").click()
-        # Заполнение
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Подтверждение создания группы
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups()
-
-    def open_groups(self):
-        wd = self.wd
-        # Открытие Групп
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        # Логин
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@type='submit']").click()
-
-
-    def open_home_page(self):
-        wd = self.wd
-        # Открытие страницы
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+        app.login(username="admin", password="secret")
+        app.create_group(Group(name="", header="", footer=""))
+        app.logout()
