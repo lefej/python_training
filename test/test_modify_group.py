1a48d83f1129c1a368c1804f8ba04df07e152c75
lisovskaya <lefej@rambler.ru>
2019-11-14 11:54:33 +0300

    ааа

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,4 +4,7 @@ def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
    # app.group.modify_first_group(Group(name="отредактированная группа", header="тест2", footer="тест2"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="отредактированная группа"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
