5829e133391cf72ac495e2bfebfd2c85029e82d0
lefej <lefej@rambler.ru>
2019-11-29 17:08:55 +0300

    добавлена проверка телефонов для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -132,10 +132,15 @@ class ContactHelper:
     def modify_first_contact(self, contact):
         self.modify_contact_by_index(0)
 
-    def modify_contact_by_index(self, contact, index):
+    def init_modify_contact_by_index(self, contact, index):
         wd = self.app.wd
         # инициировать редактирование первого конаткта
         wd.find_elements_by_xpath("//img[@title='Edit']")[index].click()
+
+    def modify_contact_by_index(self, contact, index):
+        wd = self.app.wd
+        # инициировать редактирование первого конаткта
+        self.init_modify_contact_by_index(index)
         # редактирование контакта
         self.fill(contact)
         # подтверждение редактирования
@@ -155,9 +160,25 @@ class ContactHelper:
         wd = self.app.wd
         self.go_home()
         self.contact_cache = []
-        for element in wd.find_elements_by_xpath("//tr[@name='entry']"):
-            lastname = element.find_element_by_xpath("td[2]").text
-            firstname = element.find_element_by_xpath("td[3]").text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            self.contact_cache.append(Contact(firstname = firstname, lastname = lastname, id = id))
+        for row in wd.find_elements_by_name("entry"):
+            cells = row.find_elements_by_tag_name("td")
+            lastname = cells[1].text
+            firstname = cells[2].text
+            id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+            all_phones = cells[5].text.splitlines()
+            self.contact_cache.append(Contact(firstname = firstname, lastname = lastname, id = id, home_phone=all_phones[0], mobile_phone=all_phones[1], work_phone=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.init_modify_contact_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname,
+                       home_phone=homephone, work_phone=workphone,
+                       mobile_phone=mobilephone, phone2=secondaryphone )
