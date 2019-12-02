2e80de2c6313b9c7c6b62c2470d5ce3d56874aea
lefej <lefej@rambler.ru>
2019-12-02 12:56:27 +0300

    объединены эмэйлы и телефоны

--- test/test_phones.py
+++ test/test_phones.py
@@ -5,17 +5,13 @@ def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-#def test_phones_on_contact_view_page(app):
-#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#    assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
-#    assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
-#    assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
-#    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 def clear(s):
     return re.sub("[() -]", "", s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x !="", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.home_phone, contact.mobile_phone, contact.work_phone,  contact.phone2]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x !="", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
